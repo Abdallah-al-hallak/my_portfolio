290bf9e541dbc150b758d8ffca221e114a9c804e
Abdallah al hallak <bode.1996.bh.bh@gmail.com>
2023-01-13 08:08:03 +0200

    .

--- lib/home/view/landing_page.dart
+++ lib/home/view/landing_page.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfolio/home/blocs/projects_data_bloc/bloc/projects_data_bloc.dart';
 import 'package:portfolio/home/blocs/scrolling_value/cubit/scrolling_value_cubit.dart';
-
 import 'package:portfolio/home/widgets/projects_widgets/projects_widget.dart';
-import 'package:portfolio/home/widgets/skills_widget/skills_widget.dart';
-
 import '../../animation/skills_widget_animation.dart';
 import '../widgets/about_me_widget/about_me_widget.dart';
 import '../widgets/home_widget/home_widget.dart';
